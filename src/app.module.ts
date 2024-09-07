bcd838819476748f88570164d8e020e9750a4304
Vitorgooto <101600322+Vitorgooto@users.noreply.github.com>
2024-09-07 15:44:17 -0300

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { DeckModule } from './deck/deck.module';
+import { UserModule } from './user/user.module';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/magic-api'), // Conexão com MongoDB
+    AuthModule, // Módulo de autenticação
+    DeckModule, // Módulo de decks
+    UserModule, // Módulo de usuários
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
